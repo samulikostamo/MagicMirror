d958bd443bf83effdc5b024ff22433d0721053f5
samulikostamo <samulikostamo@gmail.com>
2014-10-07 23:05:24 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ var lang = window.navigator.language;
 //change weather params here:
 //units: metric or imperial
 var weatherParams = {
-    'q':'Baarn,Netherlands',
+    'q':'Tampere,Finland',
     'units':'metric',
     'lang':lang
 };
